c01cdbe802206502701bc9b769370aa61a032e31
Your Name <you@example.com>
2025-04-20 01:52:38 +0300

    add russian_name file

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import re
 def clear_name(file_name: str) -> list:
     """Функция для очистки имен от лишних символов"""
     cleaned_names = []
-    with open(file_name, encoding='utf-8') as names_file:
+    with open('data/' + file_name, encoding='utf-8') as names_file:
         for line in names_file:
             cleaned = ''.join(c for c in line if c.isalpha())
             if cleaned:
@@ -18,7 +18,6 @@ def is_cyrillic(name_item: str) -> bool:
     return bool(re.search('[а-яА-Я]', name_item))
 
 
-
 def filter_russian_name(name_list: list) -> list:
     """Фильтрация имён написанных на русском языке"""
     new_name_list = list()
@@ -28,7 +27,14 @@ def filter_russian_name(name_list: list) -> list:
     return new_name_list
 
 
-if __name__ == '__main__':
-    names = clear_name('data/names.txt')
+def save_to_file(file_name: str, data: str) -> None:
+    """Сохраняет данные в файл"""
+    with open('data/' + file_name, 'w', encoding='utf-8') as names_file:
+        names_file.write(data)
 
-    print(filter_russian_name(names))
+
+if __name__ == '__main__':
+    names = clear_name('names.txt')
+
+    filtered_names = (filter_russian_name(names))
+    save_to_file('russian_name.txt', '\n'.join(filtered_names))
