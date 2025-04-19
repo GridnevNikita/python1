0c23c05e130d4bbef2e5b5a9bdcaf8d5ea0e4752
Your Name <you@example.com>
2025-04-20 01:31:44 +0300

    filter for russ letters

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+import re
+
+
+
 def clear_name(file_name: str) -> list:
     """Функция для очистки имен от лишних символов"""
     cleaned_names = []
@@ -8,7 +12,23 @@ def clear_name(file_name: str) -> list:
                 cleaned_names.append(cleaned)
     return cleaned_names
 
+
+def is_cyrillic(name_item: str) -> bool:
+    """Проверка на вхождение кириллицы в строку"""
+    return bool(re.search('[а-яА-Я]', name_item))
+
+
+
+def filter_russian_name(name_list: list) -> list:
+    """Фильтрация имён написанных на русском языке"""
+    new_name_list = list()
+    for name_item in name_list:
+        if is_cyrillic(name_item):
+            new_name_list.append(name_item)
+    return new_name_list
+
+
 if __name__ == '__main__':
     names = clear_name('data/names.txt')
-    for name in names:
-        print(name)
+
+    print(filter_russian_name(names))
